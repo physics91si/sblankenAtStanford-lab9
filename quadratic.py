e9da38ba6466181ae3e50be7c5ffeaea37e6755c
Scott Blankenberg <sblankenl@stanford.edu>
2016-04-28 16:02:56 -0700

    I finished lab 9

--- quadratic.py
+++ quadratic.py
@@ -15,10 +15,33 @@ def main():
 
 
 def find_roots(a,b,c):
-    mid = b^2 - 4*a*c
-    sqrt_mid = mid^(1/2)
-    x1 = (-b + sqrt_mid)/2*a
-    x2 = (-b - sqrt_mid)/2*a
+    try:
+        if (a is 0) :
+            raise TypeError()
+    except: 
+        print('a is zero')
+        return
+       
+    print(a,b,c)
+    try:
+        if(not(type(a) == int and type(b) == int and type(c) == int)):
+            raise TyperError()
+    except:
+        print('python quadratic.py physics 91si is the best')
+        return
+        
+    mid = b**2 - 4*a*c
+    try:
+        if(not(mid >= 0)):
+            raise TyperError()
+    except:
+        print('the equation has imaginary roots')
+        return
+    print(mid)
+    sqrt_mid = mid**(1/2)
+    print(sqrt_mid)
+    x1 = (-b + sqrt_mid)/(2*a)
+    x2 = (-b - sqrt_mid)/(2*a)
     return x1, x2
 
 
